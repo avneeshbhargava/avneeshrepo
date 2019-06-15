c848580479cad2f890b3f3ff2e12b8eda8da9942
Avneesh <avneeshbhargavec@gmail.com>
2019-06-16 00:41:54 +0530

    version1.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='avneeshrepo',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Avneesh',
+    author_email='',
+    description=''
+)
